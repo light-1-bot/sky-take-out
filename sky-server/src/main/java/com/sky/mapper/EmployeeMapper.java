d2ea79143861375d57b5cec6bc25f358b33c8ff1
light <123@qq.com>
2024-12-18 14:05:19 +0800

    员工分页查询代码开发

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,9 +1,12 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+
 
 @Mapper
 public interface EmployeeMapper {
@@ -25,4 +28,11 @@ public interface EmployeeMapper {
             "values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime}," +
             "#{updateTime},#{createUser},#{updateUser},#{status})")
     void insert(Employee employee);
+
+    /**
+     * 分页查询
+     * @param employeePageQueryDTO
+     * @return
+     */
+    Page<Employee> pageQuery(EmployeePageQueryDTO employeePageQueryDTO);
 }
