a1211fdc23a6bb2a2ecd305a191897d569ca3dab
light <123@qq.com>
2024-12-28 15:44:27 +0800

    商品浏览业务代码开发

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -2,6 +2,7 @@ package com.sky.service;
 
 import com.sky.dto.DishDTO;
 import com.sky.dto.DishPageQueryDTO;
+import com.sky.entity.Dish;
 import com.sky.result.PageResult;
 import com.sky.vo.DishVO;
 
@@ -59,4 +60,11 @@ public interface DishService {
      * @return
      */
     List<DishVO> getByCategoryId(Long categoryId);
+
+    /**
+     * 查询菜品
+     * @param dish
+     * @return
+     */
+    List<DishVO> list(Dish dish);
 }
--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -196,4 +196,22 @@ public class DishServiceImpl implements DishService {
         List<DishVO> list=dishMapper.getByCategoryId(categoryId);
         return list;
     }
+
+    @Override
+    public List<DishVO> list(Dish dish) {
+        List<Dish> dishList = dishMapper.list(dish);
+        List<DishVO> dishVOList = new ArrayList<>();
+
+        for (Dish d : dishList) {
+            DishVO dishVO = new DishVO();
+            BeanUtils.copyProperties(d,dishVO);
+
+            //根据菜品id查询对应的口味
+            List<DishFlavor> flavors = dishFlavorMapper.getFlavorsByDishId(d.getId());
+
+            dishVO.setFlavors(flavors);
+            dishVOList.add(dishVO);
+        }
+        return dishVOList;
+    }
 }
